74408ad1ce3ec91807dcf884c976b707f70c6d2c
rixong <rixong@gmail.com>
2020-11-10 14:11:21 -0600

    set up login route and authentication

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,3 @@
-
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -12,6 +11,7 @@ const UserSchema = new Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
     trim: true,
     lowercase: true,
     validate(value) {
@@ -35,6 +35,21 @@ const UserSchema = new Schema({
   lists: [ListSchema],
 });
 
+const User = mongoose.model('User', UserSchema);
+
+UserSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error('Unable to login.');
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error('Unable to login.');
+  }
+  return user;
+};
+
+// Hash plain text password
 UserSchema.pre('save', async function (next) {
   const user = this;
   if (user.isModified('password')) {
@@ -43,129 +58,4 @@ UserSchema.pre('save', async function (next) {
   next();
 });
 
-const User = mongoose.model('User', UserSchema);
-
 module.exports = User;
-
-///  FOR TESTING
-
-// const userId = '5fa9a647ecf09446f09659e0';
-// const produceId = '5fa9760f30188141a5d21110';
-// const meatId = '5fa9760f30188141a5d21112';
-// const apple = '5fa9763b90650b41cf091860';
-// const pork = '5fa9763b90650b41cf091861';
-// const lettuce = '5fa9763b90650b41cf091862';
-// const heb = '5fa9766e288f3242033171a2';
-// const costco = '5fa9766e288f3242033171a3';
-// const appleItem = '5fa9550942bb2e2e583b637e';
-// const porkItem = '5fa9550942bb2e2e583b637f';
-// const lettuceItem = '5fa9550942bb2e2e583b6380';
-
-// mongoose.connect('mongodb://127.0.0.1:27017/cart-compass', {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false,
-// });
-
-//ADD USER
-
-// const user1 = new User({
-//   name: 'Sammy',
-//   email: 'rixong@gmail.com',
-//   password: '125',
-// });
-// user1.categories.push(...defaultCategories);
-// user1.save()
-//   .then((user) => {
-//     console.log(user);
-//   })
-//   .catch((error) => {
-//     console.log('error here!', error);
-//   });
-
-      //ADD A FEW ITEMS TO MASTERLIST
-
-  //     User.findById(userId)
-  //       .then((user) => {
-  //         const item1 = new MasterItem({
-  //             name: 'apples',
-  //             categoryId: produceId,
-  //   });
-  //   const item2 = new MasterItem({
-  //     name: 'pork',
-  //     categoryId: meatId,
-  //   });
-  //   const item3 = new MasterItem({
-  //     name: 'lettuce',
-  //     categoryId: produceId,
-  //   });
-  //   user.masterList.push(item1);
-  //   user.masterList.push(item2);
-  //   user.masterList.push(item3);
-  //   user.save();
-  //   console.log(user.masterList);
-  // })
-  // .catch((error) => {
-  //   console.log(error);
-  // });
-
-//ADD A FEW LISTS
-
-// User.findById(userId)
-//   .then((user) => {
-//     const list11 = new List({
-//       name: 'Heb',
-//       dateCreated: Date.now(),
-//       userId,
-//       isCurrent: false,
-//     });
-//     const list12 = new List({
-//       name: 'Costco',
-//       dateCreated: Date.now(),
-//       userId,
-//       isCurrent: false,
-//     });
-//     user.lists.push(list11);
-//     user.lists.push(list12);
-//     user.save();
-//     console.log('Done', user.lists);
-//   })
-
-// ADD A FEW ITEMS TO HEB LIST
-
-// User.findById(userId)
-//   .then((user) => {
-//     const myList = user.lists.id(heb);
-//     const item1 = new ListItem({
-//       masterItemId: apple,
-//       quantity: '1 dozen',
-//     });
-//     const item2 = new ListItem({
-//       masterItemId: pork,
-//       quantity: '2 lbs',
-//     });
-//     const item3 = new ListItem({
-//       masterItemId: lettuce,
-//       quantity: '6 stalks',
-//     });
-//     myList.listItems.push(item1);
-//     myList.listItems.push(item2);
-//     myList.listItems.push(item3);
-//     user.save();
-//     console.log(myList.listItems);
-//   })
-
-///TEST QUERIES
-
-// User.findById(userId)
-//   .then((user) => {
-//     const myList = user.lists.id(heb);
-//     const myItem = myList.listItems.id(appleItem);
-//     console.log(myItem);
-//   });
-
-// List.findById(heb)
-//   .then((list) => {
-//     console.log(list);
-//   });
