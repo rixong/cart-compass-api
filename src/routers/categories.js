f59f2c1eb47b82e895d0463f6a33d2dc8e46b8cb
rixong <rixong@gmail.com>
2020-11-22 17:53:26 -0600

    auth bug

--- src/routers/categories.js
+++ src/routers/categories.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const auth = require('../middleware/authentication');
+const { Category } = require('../models/category');
 
 const router = new express.Router();
 
-router.get('/categories', auth, async (req, res) => {
+router.get('/categories', async (req, res) => {
   try {
-    res.status(201).send(req.curUser.categories);
+    const categories = Category.find({});
+    console.log(categories)
+    res.status(201).send(categories);
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-router.patch('/categories', auth, async (req, res) => {
-  try {
-    const category = req.curUser.categories.id(req.body.categoryId);
-    console.log(category);
-    res.status(201).send();
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
+// router.patch('/categories', auth, async (req, res) => {
+//   try {
+//     const category = req.curUser.categories.id(req.body.categoryId);
+//     console.log(category);
+//     res.status(201).send();
+//   } catch (e) {
+//     res.status(400).send(e);
+//   }
+// });
 
 module.exports = router;
